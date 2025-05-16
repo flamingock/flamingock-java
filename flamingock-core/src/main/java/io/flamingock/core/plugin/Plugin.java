f6fe99f72842ed1b7f26cd878dac481f6f46d311
Rubén Berciano <bercianor@users.noreply.github.com>
2025-05-16 05:23:57 +0000

    chore: grouped housekeeping tasks

---
+++ flamingock-core/src/main/java/io/flamingock/core/plugin/Plugin.java
@@ -0,0 +1,26 @@
+package io.flamingock.core.plugin;
+
+import io.flamingock.core.context.ContextInitializable;
+import io.flamingock.core.event.EventPublisher;
+import io.flamingock.core.context.DependencyContext;
+import io.flamingock.core.task.filter.TaskFilter;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public interface Plugin extends ContextInitializable {
+
+    default Optional<EventPublisher> getEventPublisher() {
+        return Optional.empty();
+    }
+
+    default Optional<DependencyContext> getDependencyContext() {
+        return Optional.empty();
+    }
+
+    default List<TaskFilter> getTaskFilters() {
+        return Collections.emptyList();
+    }
+
+}
