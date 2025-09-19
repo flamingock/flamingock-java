aeadd5c4669ea34c777ef05c0d6e0e2bbb3698ec
davidfrigolet <dfrigolet@gmail.com>
2025-09-19 06:54:50 +0100

    fix: dyanmodb-enhanced version in target system and utils (#375)

--- utils/dynamodb-util/build.gradle.kts
+++ utils/dynamodb-util/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 val jacksonVersion = "2.16.0"
 dependencies {
 
-    api("software.amazon.awssdk:dynamodb-enhanced:2.12.0")
+    api("software.amazon.awssdk:dynamodb-enhanced:2.25.29")
     
     // TestKit dependencies
     api(project(":utils:test-util"))
