dfae911aa64a32bd61f6851766de282f54cf3b40
bercianor <bercianor@gmail.com>
2025-10-22 17:31:52 +0100

    fix: license header in SqlAuditMarkerDialectHelperTest class

--- core/target-systems/sql-target-system/src/test/java/io/flamingock/targetsystem/sql/SqlAuditMarkerDialectHelperTest.java
+++ core/target-systems/sql-target-system/src/test/java/io/flamingock/targetsystem/sql/SqlAuditMarkerDialectHelperTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.flamingock.targetsystem.sql;
 
 import com.zaxxer.hikari.HikariConfig;
