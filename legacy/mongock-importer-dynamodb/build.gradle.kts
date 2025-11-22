e409f7e31aa08bfc50624d2b0805b746e131d057
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-11-22 14:38:19 +0000

    feat: Mongock import to FlamingockCommunity(WIP: dynamodb)

--- legacy/mongock-importer-dynamodb/build.gradle.kts
+++ legacy/mongock-importer-dynamodb/build.gradle.kts
@@ -3,6 +3,22 @@ dependencies {
     implementation(project(":utils:dynamodb-util"))
 
     compileOnly("software.amazon.awssdk:dynamodb-enhanced:2.25.29")
+
+    testAnnotationProcessor(project(":core:flamingock-processor"))
+    testAnnotationProcessor(project(":legacy:mongock-support"))
+    testImplementation(project(":legacy:mongock-support"))
+
+    testImplementation(project(":community:flamingock-auditstore-dynamodb"))
+
+
+    testImplementation(project(":utils:test-util"))
+
+    testImplementation("org.testcontainers:junit-jupiter:1.18.3")
+    testImplementation("org.mockito:mockito-inline:4.11.0")
+    testImplementation("org.testcontainers:localstack:1.19.7")
+    testImplementation("software.amazon.awssdk:dynamodb:2.25.61")
+
+    testImplementation("org.mockito:mockito-inline:4.11.0")
 }
 
 
--- legacy/mongock-importer-dynamodb/src/test/java/io/flamingock/importer/mongock/dynamodb/changes/MongockChange2.java
+++ legacy/mongock-importer-dynamodb/src/test/java/io/flamingock/importer/mongock/dynamodb/changes/MongockChange2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Flamingock (https://www.flamingock.io)
+ * Copyright 2023 Flamingock (https://www.flamingock.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,14 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.flamingock.importer.mongock.mongodb.legacy;
+package io.flamingock.importer.mongock.dynamodb.changes;
 
-import io.flamingock.api.annotations.Apply;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 
-@ChangeUnit(id = "client-initializer", order = "1", author = "flamingock-team")
-public class MongockChange1 {
+@ChangeUnit(id = "client-updater", order = "2", author = "flamingock-team")
+public class MongockChange2 {
 
     @Execution
     public void apply() {
