1b4541a9e9c9c54c5ee2f983a73538859434b669
davidfrigolet <dfrigolet@gmail.com>
2025-12-11 13:16:22 +0000

    feat: flamingock test support DefaultExceptionValidator

--- core/flamingock-test-support/src/main/java/io/flamingock/support/validation/impl/DefaultExceptionValidator.java
+++ core/flamingock-test-support/src/main/java/io/flamingock/support/validation/impl/DefaultExceptionValidator.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2025 Flamingock (https://www.flamingock.io)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.flamingock.support.validation.impl;
 
 import io.flamingock.support.validation.ExceptionValidator;
+import io.flamingock.support.validation.error.ExceptionNotExpectedError;
+import io.flamingock.support.validation.error.ExceptionTypeMismatchError;
 import io.flamingock.support.validation.error.ValidationResult;
 
 import java.util.function.Consumer;
@@ -40,7 +27,30 @@ public class DefaultExceptionValidator implements ExceptionValidator {
 
     @Override
     public ValidationResult validate(Throwable actualException) {
-        // TODO: Implement actual validation logic
+        // No exception expected
+        if (expectedExceptionClass == null) {
+            if (actualException == null) {
+                return ValidationResult.success(VALIDATOR_NAME);
+            } else {
+                return ValidationResult.failure(VALIDATOR_NAME, new ExceptionNotExpectedError(actualException));
+            }
+        }
+
+        // An exception is expected but none was thrown
+        if (actualException == null) {
+            return ValidationResult.failure(VALIDATOR_NAME,
+                    new ExceptionTypeMismatchError(expectedExceptionClass, null));
+        }
+
+        // Type mismatch
+        if (!expectedExceptionClass.isInstance(actualException)) {
+            return ValidationResult.failure(VALIDATOR_NAME,
+                    new ExceptionTypeMismatchError(expectedExceptionClass, actualException.getClass()));
+        }
+
+        if (expectedExceptionConsumer != null) {
+            expectedExceptionConsumer.accept(actualException);
+        }
         return ValidationResult.success(VALIDATOR_NAME);
     }
 }
