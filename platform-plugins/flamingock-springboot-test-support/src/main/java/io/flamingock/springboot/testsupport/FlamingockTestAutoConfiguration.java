1849bbe7b40b74d79159a7005fa493b72a6b99a2
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-12-23 21:18:18 +0000

    refactor: remoeved ConditionalOnBean and renamed FlamingockSpringBootTestSupport bean

--- platform-plugins/flamingock-springboot-test-support/src/main/java/io/flamingock/springboot/testsupport/FlamingockTestAutoConfiguration.java
+++ platform-plugins/flamingock-springboot-test-support/src/main/java/io/flamingock/springboot/testsupport/FlamingockTestAutoConfiguration.java
@@ -16,7 +16,6 @@
 package io.flamingock.springboot.testsupport;
 
 import io.flamingock.internal.core.builder.AbstractChangeRunnerBuilder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -70,8 +69,7 @@ public class FlamingockTestAutoConfiguration {
      */
     @Bean
     @Scope("prototype")
-    @ConditionalOnBean(AbstractChangeRunnerBuilder.class)
-    public FlamingockSpringBootTestSupport flamingockGivenStage(AbstractChangeRunnerBuilder<?, ?> builderFromContext) {
+    public FlamingockSpringBootTestSupport flamingockTestSupport(AbstractChangeRunnerBuilder<?, ?> builderFromContext) {
         return new FlamingockSpringBootTestSupport(builderFromContext);
     }
 }
