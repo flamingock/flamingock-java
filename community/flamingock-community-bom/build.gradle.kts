160bc5d2d674de2e825b17f439f0c95ea3caea84
bercianor <bercianor@gmail.com>
2026-02-03 00:38:55 +0000

    feat: remove SQL and MongoDB Sync templates as they have their own repo now

--- community/flamingock-community-bom/build.gradle.kts
+++ community/flamingock-community-bom/build.gradle.kts
@@ -12,8 +12,6 @@ dependencies {
         api("io.flamingock:flamingock-auditstore-dynamodb:$version")
         api("io.flamingock:flamingock-auditstore-sql:$version")
         api("io.flamingock:flamingock-test-support:${version}")
-        api("io.flamingock:flamingock-sql-template:$version")
-        api("io.flamingock:flamingock-mongodb-sync-template:${version}")
         api("io.flamingock:flamingock-springboot-integration:${version}")
         api("io.flamingock:flamingock-springboot-test-support:${version}")
         api("io.flamingock:flamingock-graalvm:${version}")
