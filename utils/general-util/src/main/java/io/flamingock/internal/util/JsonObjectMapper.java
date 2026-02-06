36ee9f4652b6fedcc9864f37f55fe2772e2e0a9a
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2026-02-06 16:15:41 +0000

    fix: cli date fixed

--- utils/general-util/src/main/java/io/flamingock/internal/util/JsonObjectMapper.java
+++ utils/general-util/src/main/java/io/flamingock/internal/util/JsonObjectMapper.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import static com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS;
 
@@ -30,11 +31,11 @@ public final class JsonObjectMapper {
 
     public final static ObjectMapper DEFAULT_INSTANCE = JsonMapper.builder()
             .addModule(new Jdk8Module())
+            .addModule(new JavaTimeModule())
             .enable(ACCEPT_CASE_INSENSITIVE_ENUMS)
             .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
             .serializationInclusion(JsonInclude.Include.NON_NULL)
-//            .registerModule(JavaTimeModule()) //No needed for now
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .disable(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS)
             .build();
