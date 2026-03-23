a32b47053b7150717ce415c566c52507617a68b0
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2026-03-24 07:19:20 +0800

    chore: bump version to 1.2.0-beta.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 allprojects {
     group = "io.flamingock"
-    version = "1.2.0-beta.5"
+    version = "1.2.0-beta.6"
 
     extra["templateApiVersion"] = "1.3.1"
     extra["generalUtilVersion"] = "1.5.0"
