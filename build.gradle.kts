7fbb53850fba81412ea2faa39501f0de5b8852e6
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2026-05-01 11:21:18 +0100

    chore: updated flamingock library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,11 @@ allprojects {
     group = "io.flamingock"
     version = "1.3.0-SNAPSHOT"
 
-    extra["generalUtilVersion"] = "1.5.2"
-    extra["templateApiVersion"] = "1.3.2"
-    extra["coreApiVersion"] = "1.3.1"
-    extra["sqlVersion"] = "1.2.0"
-    extra["mongodbTemplateVersion"] = "1.3.0-SNAPSHOT"
+    extra["generalUtilVersion"] = "1.5.3"
+    extra["templateApiVersion"] = "1.3.3"
+    extra["coreApiVersion"] = "1.3.2"
+    extra["sqlVersion"] = "1.3.1"
+    extra["mongodbTemplateVersion"] = "1.3.1"
 
     repositories {
         mavenLocal()
