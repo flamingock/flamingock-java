d947f1e510b7aea6164ad8fc20e4ac10f2e0e7fe
Antonio Pérez <aperezdieppa@gmail.com>
2026-07-21 15:02:00 +0200

    refactor: executioRuntime package

--- core/flamingock-core/src/main/java/io/flamingock/internal/core/runtime/ExecutionRuntime.java
+++ core/flamingock-core/src/main/java/io/flamingock/internal/core/runtime/ExecutionRuntime.java
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.flamingock.internal.common.core.context;
+package io.flamingock.internal.core.runtime;
+
+import io.flamingock.internal.common.core.context.ExecutionContext;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
